e1b86695e4965925f4156e8dff18db4c06fc4deb
Alex <nimych01@gmail.com>
2020-02-15 18:57:43 +0200

    Чистка кода (удаление лишних комментариев)

--- func.php
+++ func.php
@@ -2,24 +2,14 @@
     function isAlgorithm($string)
     {
         $parentheses = array('{', '}', '(', ')', '[', ']');
-        //$stacks = array_intersect(str_split($string), $parentheses);
-        $stacks = array_intersect(array_filter(str_split($string)), $parentheses);
-        //var_dump($stacks);
-       /* foreach ($stacks as $stack) {
-            echo $stack;
-        }
-        echo "<br>";*/
-        if (sizeof($stacks)==0){
-            return $message = "В строке нет скобок!";
-        }
+        $stacks = array_intersect(str_split($string), $parentheses);
+        if (sizeof($stacks)==0) return $message = "В строке нет скобок.";
         elseif (sizeof($stacks)%2==0){
-            //echo "Вычисления" ."<br>";
             $i = 0;
             $stacks = array_values($stacks);
             while ($i < sizeof($stacks)) {
                 $key = array_search($stacks[$i], $parentheses);
                 if (array_key_exists($i, $stacks) && $stacks[$i] == $parentheses[$key] && $stacks[$i + 1] == $parentheses[$key+1]) {
-                    //echo "Удаляем";
                     unset($stacks[$i], $stacks[$i + 1]);
                     $stacks = array_values($stacks);
                     $i = 0;
@@ -28,17 +18,8 @@
             if (sizeof($stacks) == 0) return $message = "Верно:)";
             else return $message = "Не верно!";
         }
-
-        else{
-            return $message = "Не верно!";
-        }
-        /*echo "<br>";
-        foreach ($stacks as $stack) {
-            echo $stack;
-        }
-        echo "<br>";*/
+        else return $message = "Не верно!";
     }
-//echo isAlgorithm("[]462j{j{781}}(2dh)((4))f([]843g)m{3{[()c9]0}g4}");
 $message = isAlgorithm($_POST['string']);
 header("Location: /?info=".$message);
 
