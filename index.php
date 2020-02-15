601ad1cd15773b2882e08807f4e2a441672cff04
Alex <nimych01@gmail.com>
2020-02-15 16:45:41 +0200

    Создал фому + стили

--- index.php
+++ index.php
@@ -5,9 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Task-Wezom</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+<form action="#" method="POST" class="ui-form">
+    <div class="form-row">
+        <input type="text" name="string" id="string" required autocomplete="off"><label for="string">выражение:</label>
+    </div>
+    <div class="form-row">
+        <span>##</span>
+    </div>
+    <p><input type="submit" value="Проверить"></p>
+</form>
 </body>
 </html>
