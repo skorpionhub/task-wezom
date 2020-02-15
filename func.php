ccad9e005fee2d39f43753f7cacb71d25ce03842
Alex <nimych01@gmail.com>
2020-02-15 18:30:03 +0200

    Алгоритм (добавил условие на вывод сообщений)

--- func.php
+++ func.php
@@ -10,32 +10,36 @@
         }
         echo "<br>";*/
         if (sizeof($stacks)==0){
-            echo "В строке нет скобок!";
+            return $message = "В строке нет скобок!";
         }
         elseif (sizeof($stacks)%2==0){
-            echo "Вычисления" ."<br>";
+            //echo "Вычисления" ."<br>";
             $i = 0;
             $stacks = array_values($stacks);
             while ($i < sizeof($stacks)) {
                 $key = array_search($stacks[$i], $parentheses);
                 if (array_key_exists($i, $stacks) && $stacks[$i] == $parentheses[$key] && $stacks[$i + 1] == $parentheses[$key+1]) {
-                    echo "Удаляем";
+                    //echo "Удаляем";
                     unset($stacks[$i], $stacks[$i + 1]);
                     $stacks = array_values($stacks);
                     $i = 0;
                 } else $i++;
             }
+            if (sizeof($stacks) == 0) return $message = "Верно:)";
+            else return $message = "Не верно!";
         }
 
         else{
             echo "Не верно!";
         }
-        echo "<br>";
+        /*echo "<br>";
         foreach ($stacks as $stack) {
             echo $stack;
         }
-        echo "<br>";
+        echo "<br>";*/
     }
-isAlgorithm("[]462j{j{781}}(2dh)((4))f([]843g)m{3{[()c9]0}g4}");
+echo isAlgorithm("[]462j{j{781}}(2dh)((4))f([]843g)m{3{[()c9]0}g4}");
+
+
 
 ?>
